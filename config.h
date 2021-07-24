3b673f08ca60f595ee33d50b68bae86ecf800410
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-07-23 23:27:12 -0400

    Add files via upload

--- config.h
+++ config.h
@@ -13,10 +13,17 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#002b36";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#fdf6e3";
+static char selfgcolor[]            = "#fdf6e3";
+static char selbordercolor[]        = "#fdf6e3";
+static char selbgcolor[]            = "#073642";
+
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -59,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normbordercolor, "-sb", normfgcolor, "-sf", selbordercolor, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
 static const char *browser[]  = { "chromium", NULL };
 static const char *discord[]  = { "discord", NULL };
