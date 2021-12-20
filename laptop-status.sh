6e63ac4f32f020fff625f3de2ee7fadaa8305817
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-12-20 14:30:35 -0500

    Update laptop-status.sh

--- laptop-status.sh
+++ laptop-status.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cowsay -f tux uwm status script
+echo "uwm status script"
 
 status () {
-	echo -n "$(date '+%Y-%m-%d %H:%M') "
-	echo -n "Battery: $(acpi | awk '{print $4}' | sed s/,//)"
+	echo -n "$(date '+%Y-%m-%d | %H:%M') | "
+	echo -n "Battery: $(acpi | awk '{print $4}' | sed s/,//) | "
+	echo -n "RAM $(free -m | grep '^Mem' | awk '{print "used: " $3"MB"}')"
 }
 
 while :; do
@@ -13,4 +14,3 @@ while :; do
 	sleep 1
 
 done
-
